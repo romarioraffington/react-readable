30be1c23e3d93ef8ff9ee6bbc982a7267bd07edc
Romario Raffington <romarioraff@gmail.com>
2017-08-27 22:39:21 -0500

    feat: Created the Header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+// Extneral Dependencies;
+import React from 'react';
+
+// Our Dependencies
+import styles from './index.scss';
+
+export default () => (
+  <div className='top-container'>
+    <a className='logo'>Readable</a>
+    <a className="add-post">Add Post</a>
+  </div>   
+)
