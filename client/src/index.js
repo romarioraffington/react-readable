19c5158380a34519704b70bb22252567f90ae10f
Romario Raffington <romarioraff@gmail.com>
2017-08-27 22:41:34 -0500

    feat: Make it so the Home component is the first component being rendered

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+// External Dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Our Dependencies
+import Home from './screens/Home';
+import styles from './index.scss';
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('app')
+);
