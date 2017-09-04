5e69733bb794feccc8e9ee1957c861de6a2798de
Romario Raffington <romarioraff@gmail.com>
2017-09-04 17:19:21 -0500

    refactor: Made App a smart component and made Home a dumb component since other screens are going to depend on the App component to past props similar to the props being passed to Home (e.g posts)

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,15 +1,64 @@
 // External Dependencies
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 // Our Components
-import Home from 'src/screens/Home'
+import Home from 'src/screens/Home';
+
+// Our Actions
+import { fetchPosts, filterPost } from 'src/components/Post/model/actions';
+import { fetchCategories } from 'src/screens/Home/components/Category/model/actions';
+
+// Redux
+const mapStateToProps = ({ post, home, router }) => ({
+  posts: post.posts,
+  isFetchingPosts: post.isFetching,
+  categories: home.category.categories,
+  isFetchingCategories: home.category.isFetching,
+  pathname: router.location.pathname,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onMount: () => {
+    dispatch(fetchPosts());
+    dispatch(fetchCategories());
+  },
+  onFilterClick: (order, by) => dispatch(filterPost(order, by)),
+});
 
 class App extends Component {
+  componentWillMount() {
+    this.props.onMount();
+  }
+
   render() {
+    const {
+      posts,
+      isFetchingPosts,
+      categories,
+      isFetchingCategories,
+      pathname,
+      onFilterClick
+     } = this.props;
+
+     const filterClick = (order, by) => {
+      onFilterClick(order, by);
+    }
+
     return (
-      <Home/>
-    )  
+      <Home 
+        posts={posts}
+        isFetchingPosts={isFetchingPosts}
+        categories={categories}
+        isFetchingCategories={isFetchingCategories}
+        onFilterClick={onFilterClick}
+        pathname={pathname}
+      />
+    )
   }
 }
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- client/src/screens/Home/index.js
+++ client/src/screens/Home/index.js
@@ -9,54 +9,25 @@ import Category from './components/Category';
 import Filter from 'src/components/Filter';
 import Post from 'src/components/Post';
 
-// Our Actions
-import { fetchPosts, filterPost } from 'src/components/Post/model/actions';
-import { fetchCategories } from './components/Category/model/actions';
-
-// Redux
-const mapStateToProps = ({ post, home, router }) => ({
-  posts: post.posts,
-  isFetchingPosts: post.isFetching,
-  categories: home.category.categories, 
-  isFetchingCategories: home.category.isFetching, 
-  pathname: router.location.pathname, 
-})
-
-const mapDispatchToProps = (dispatch) => ({
-  onMount: () => {
-    dispatch(fetchPosts());
-    dispatch(fetchCategories());
-  },
-  onFilterClick: (order, by) => dispatch(filterPost(order, by)),
-})
-
 // Component
 class Home extends Component {
-  componentWillMount() {
-    this.props.onMount();
-  }
-  
   render() {
-    const { 
-      posts, 
+
+    const {
+      posts,
       isFetchingPosts,
       categories,
       isFetchingCategories,
       pathname,
-      onFilterClick
-     } = this.props;
+      onFilterClick,
+      } = this.props;
 
-     // Filter Posts based on the pathname 
-     // returned by react-router-redux
-     let filteredPosts = posts;
-     if (pathname !== '/') {
-        filteredPosts = posts.filter(p => 
-          p.category.toLowerCase() === pathname.replace('/','')
-        )
-     }
-
-     const filterClick = (order, by) => {
-      onFilterClick(order, by);
+    // Filter Posts based on the pathname 
+    let filteredPosts = posts;
+    if (pathname !== '/') {
+      filteredPosts = posts.filter(p =>
+        p.category.toLowerCase() === pathname.replace('/', '')
+      )
     }
 
     return (
@@ -64,7 +35,7 @@ class Home extends Component {
         <Header />
         <div className="main-container">
           <div className="left-container">
-            <Category 
+            <Category
               categories={categories}
               isFetching={isFetchingCategories}
             />
@@ -76,15 +47,12 @@ class Home extends Component {
             />
           </div>
           <div className="right-container">
-            <Filter onFilterClick={filterClick} />
+            <Filter onFilterClick={onFilterClick} />
           </div>
         </div>
-      </div> 
-    )  
+      </div>
+    )
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Home);
+export default Home;
