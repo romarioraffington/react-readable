24ba4c07044d398b0a0fc6f0bd0c989913ff1f93
Romario Raffington <romarioraff@gmail.com>
2017-09-03 23:12:23 -0500

    feat: Made it so that when the logo is clicked the user to redirected to the index route

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,13 @@
 // Extneral Dependencies;
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Our Dependencies
 import styles from './index.scss';
 
 export default () => (
   <div className='top-container'>
-    <a className='logo'>Readable</a>
+    <Link to='/' className='logo'>Readable</Link>
     <a className="add-post">Add Post</a>
   </div>   
 )
