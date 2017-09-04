1d982781dc5f387221abf062c8e85952260e1003
Romario Raffington <romarioraff@gmail.com>
2017-09-03 23:09:55 -0500

    refactor: Added Post models to the post folder instead of a root model folder

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -4,7 +4,7 @@ import { routerReducer } from 'react-router-redux'
 
 // Our Dependencies
 import HomeReducer from 'src/screens/Home/reducer';
-import post from 'src/models/Post/reducer';
+import post from 'src/components/Post/model/reducer';
 
 export default combineReducers({
   router: routerReducer,
