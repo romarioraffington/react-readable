6a3c11fc1ef98580d547fa42ed0500df4f29efb4
Romario Raffington <romarioraff@gmail.com>
2017-09-14 18:52:02 -0500

    chore: Added a postbuild and prebuild script for heroku

---
+++ scripts/heroku-postbuild.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+export NPM_CONFIG_PRODUCTION=false
+export NPM_CONFIG_PRODUCTION=true; 
+export NODE_ENV=production;
---
+++ scripts/heroku-prebuild.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+export NPM_CONFIG_PRODUCTION=false
+export NODE_ENV=; NPM_CONFIG_PRODUCTION=false 
+export NODE_ENV=development 
+npm i
