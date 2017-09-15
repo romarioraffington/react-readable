7d8e4f4562b994697d49f0c2bfd8007dd8f4e7f6
Romario Raffington <romarioraff@gmail.com>
2017-09-14 21:05:16 -0500

    chore: Removed the heroku build scripts

--- scripts/heroku-postbuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-export NPM_CONFIG_PRODUCTION=false
-export NPM_CONFIG_PRODUCTION=true; 
-export NODE_ENV=production;
--- scripts/heroku-prebuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-export NPM_CONFIG_PRODUCTION=false
-export NODE_ENV=; NPM_CONFIG_PRODUCTION=false 
-export NODE_ENV=development 
-npm i
-npm run build
